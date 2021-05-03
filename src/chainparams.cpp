4817bf9eda3c50541bff3ad81ae28703a7ecea5d
Thomas <ThomasTail@users.noreply.github.com>
2021-05-03 10:35:44 -0400

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -121,7 +121,7 @@ public:
         genesis = CreateGenesisBlock(1619971641, 734535071, 0x1f0fffff, 1, 2 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
         assert(consensus.hashGenesisBlock == uint256S("0xcd444f191d7c0541f167fe7443b07bc019ea92e59dd45c3b7b0eb7852ebf66ca"));
-        assert(genesis.hashMerkleRoot == uint256S("0xcd444f191d7c0541f167fe7443b07bc019ea92e59dd45c3b7b0eb7852ebf66ca"));
+        assert(genesis.hashMerkleRoot == uint256S("0xaa7b8d781373cd0d87c783b9e7694446f2e16115c1eaefd69437c8a002aaaba9"));
 
         // Note that of those with the service bits flag, most only support a subset of possible options
         //vSeeds.emplace_back("seed-a.gamecoin.loshan.co.uk", true);
